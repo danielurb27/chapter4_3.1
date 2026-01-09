2c88931c02fe75d5afa6ef428eabc6cabd45ce63
Daniel <urbandanielsp@gmail.com>
2026-01-09 17:42:59 -0300

    change message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  res.send('<h1>Hello from CI/CD pipeline v2!</h1>')
 })
 
 const PORT = process.env.PORT || 3000;
